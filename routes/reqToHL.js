dd3b10f8cde25941139973b9669dfb8f49ad7b6a
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2020-07-23 14:32:12 +0200

    modifed with email

--- routes/reqToHL.js
+++ routes/reqToHL.js
@@ -1,29 +1,30 @@
-const express = require('express')
-const router = express.Router()
-const {sendInfoToOneServer} = require('../amqp/producer.js')
+const express = require("express");
+const router = express.Router();
+const { sendInfoToOneServer } = require("../amqp/producer.js");
 
-router.post('/', async (req,res) => {
-    var data = new Date();
-    var gg, mm, aaaa;
-    gg = data.getDate() + "/";
-    mm = data.getMonth() + 1 + "/";
-    aaaa = data.getFullYear();
+router.post("/", async (req, res) => {
+  var data = new Date();
+  var gg, mm, aaaa;
+  gg = data.getDate() + "/";
+  mm = data.getMonth() + 1 + "/";
+  aaaa = data.getFullYear();
 
-    var objToSend = {
-        name: req.body.name,
-        surname: req.body.surname,
-        birthday: req.body.day + '/'+ req.body.month + '/'+req.body.year,
-        day: gg + mm + aaaa,
-        street: req.body.street,
-        city: req.body.city,
-        cap: req.body.cap,
-        to: req.body.to,
-        service: req.body.service
-    }
-    
-    sendInfoToOneServer(objToSend)
+  var objToSend = {
+    name: req.body.name,
+    surname: req.body.surname,
+    email: req.body.email,
+    birthday: req.body.day + "/" + req.body.month + "/" + req.body.year,
+    day: gg + mm + aaaa,
+    street: req.body.street,
+    city: req.body.city,
+    cap: req.body.cap,
+    to: req.body.to,
+    service: req.body.service,
+  };
 
-    res.send(objToSend)
-})
+  sendInfoToOneServer(objToSend);
 
-module.exports = router
+  res.send(objToSend);
+});
+
+module.exports = router;
