87b1d99479624828a1d27ebcec4bd89ee2ac2476
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-07-31 17:04:43 +0200

    added cors and body parser settingws

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+var cors = require("cors");
+var bodyParser = require("body-parser");
 
-require('./startup/routes')(app)
+app.use(cors());
+//setting bodyparser
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 
+require("./startup/routes")(app);
 
-const port = process.env.PORT || 8080
-const server = app.listen(8080, () =>  { console.log("Server listening on port : " , port)})
+const port = process.env.PORT || 8080;
+const server = app.listen(8080, () => {
+  console.log("Server listening on port : ", port);
+});
 
-
-module.exports = server
+module.exports = server;
