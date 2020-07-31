a51f5f5108ff9f5ba26af375f9d5989c3a53e4d9
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2020-07-31 18:54:23 +0200

    added id for userData in trasaction and updated config with services' list

--- blockchain/blockchain.js
+++ blockchain/blockchain.js
@@ -8,7 +8,7 @@ function Blockchain() {
   this.pendingTransactions = [];
 
   this.currentNodeUrl = currentNodeUrl;
-  this.networkNodes = [];
+  this.networkNodes = [config.get('homel_2')];
 
   this.createNewBlock(0, "0", "0"); // Genesis Block
 }
