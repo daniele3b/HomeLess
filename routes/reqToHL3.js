76339e187add49c79acda19e7375a0559db47ec7
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2020-08-06 14:24:38 +0200

    fixed service 3

--- routes/reqToHL3.js
+++ routes/reqToHL3.js
@@ -16,7 +16,34 @@ var storage = multer.diskStorage({
 
 var upload = multer({ storage: storage });
 
-router.post("/service3", upload.single("file"), async (req, res, next) => {
+router.post("/en/service3", upload.single("file"), async (req, res, next) => {
+  const file = req.file;
+  if (!file) {
+    res.status(400).send("Bad request!");
+  }
+
+  //Call the verify service after have uploaded the file
+
+  const options = {
+    url: config.get("currentNodeUrl") + config.get("port") + "/verifyDocument",
+    method: "post",
+    data: {
+      pdfId: req.body.pdf_id,
+      path: "./uploads/" + file.originalname,
+    },
+  };
+  axios(options)
+    .then((val) => {
+      deletePdf("./uploads/" + file.originalname);
+      res.status(200).send("Document verified!");
+    })
+    .catch((err) => {
+      deletePdf("./uploads/" + file.originalname);
+      res.status(400).send("Document not verified!");
+    });
+});
+
+router.post("/it/service3", upload.single("file"), async (req, res, next) => {
   const file = req.file;
   if (!file) {
     res.status(400).send("Bad request!");
