a492ccd6e9f4fc2a06103fd5caab9d29a297e9f8
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-08-09 17:15:58 +0200

    fix

--- helper/digitalSignatureRSA.js
+++ helper/digitalSignatureRSA.js
@@ -5,7 +5,6 @@ require("dotenv").config();
 
 function verifyHashRSA(publicKey, file, signature) {
   const doc = fs.readFileSync(file);
-  console.log("DIO CANE");
 
   const isVerified = crypto.verify(
     "sha256",
