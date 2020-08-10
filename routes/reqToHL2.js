db5b38fbad571dbb3335a07560991e2a022dfc76
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2020-08-10 12:51:19 +0200

    code refactoring reqToHL2

--- routes/reqToHL2.js
+++ routes/reqToHL2.js
@@ -5,113 +5,147 @@ const axios = require("axios");
 const config = require("config");
 
 router.post("/en/service2", async (req, res) => {
-  var data = new Date();
-  var gg, mm, aaaa;
-  gg = data.getDate() + "/";
-  mm = data.getMonth() + 1 + "/";
-  aaaa = data.getFullYear();
 
-  const strday = req.body.day.split("-")
+  // Check if service is active
+  const options = {
+    url: config.get("homel_2") + "/getStatusServer",
+    method: "get"
+  }
+  
+  axios(options)
+  // If service is active
+  .then(response => {
+
+    // I create data for amqp producer
+    var data = new Date();
+    var gg, mm, aaaa;
+    gg = data.getDate() + "/";
+    mm = data.getMonth() + 1 + "/";
+    aaaa = data.getFullYear();
+
+    const strday = req.body.day.split("-")
     const day = strday[2]
     const month = strday[1]
     const year = strday[0]
 
-  var objToSend = {
-    name: req.body.name,
-    surname: req.body.surname,
-    email: req.body.email,
-    day: day + "/" + month + "/" + year,
-    street: req.body.street,
-    cash: req.body.cash,
-    service: "2",
-    lang: "eng"
-  };
-
-  if (config.get("security_active") == "yes") {
-    const options = {
-      url: config.get("homel_2") + "/getStatusServer",
-      method: "get",
+    var objToSend = {
+      name: req.body.name,
+      surname: req.body.surname,
+      email: req.body.email,
+      day: day + "/" + month + "/" + year,
+      street: req.body.street,
+      cash: req.body.cash,
+      service: "2",
+      lang: "eng"
     };
 
-    axios(options)
-      .then((response) => {
-        // Getting publicKeys array
-        const { publicKeys } = require("../startup/getPublicKey");
+    // If security is active
+    if (config.get("security_active") == "yes") {
+      
+      // Getting publicKeys array
+      const { publicKeys } = require("../startup/getPublicKey");
 
-        if (response.data.encoding == "symmetric") publicKeys[0] = "symmetric";
+      if (response.data.encoding == "symmetric") publicKeys[0] = "symmetric";
 
-        sendInfoToOneServer(objToSend);
-        res.redirect(config.get("currentNodeUrl") + config.get("port") +'/en/messageSendingSuccess')
-      })
-      .catch((err) => {
-        // Getting publicKeys array
-        const { publicKeys } = require("../startup/getPublicKey");
+      sendInfoToOneServer(objToSend);
+      res.redirect(config.get("currentNodeUrl") + config.get("port") +'/en/messageSendingSuccess')
+    }
 
-        console.log(config.get("homel_2") + " is offline!");
+    // If security is not active
+    else{
+      sendInfoToOneServer(objToSend);
+      res.redirect(config.get("currentNodeUrl") + config.get("port") +'/en/messageSendingSuccess')
+    }   
+  })
+  
+  // If service is not active
+  .catch(err => {
 
-        publicKeys[0] = "NA";
+    // If security is active
+    if(config.get("security_active") == "yes"){
+      
+      // Getting publicKeys array
+      const { publicKeys } = require("../startup/getPublicKey");
 
-        res.redirect(config.get("currentNodeUrl") + config.get("port") +'/en/messageSendingError')
-      });
-  } else {
-    sendInfoToOneServer(objToSend);
-    res.send(objToSend);
-  }
+      console.log(config.get("homel_2") + " is offline!");
+
+      publicKeys[0] = "NA";
+    }
+
+    res.redirect(config.get("currentNodeUrl") + config.get("port") +'/en/messageSendingError')
+  })
+
 });
 
 router.post("/it/service2", async (req, res) => {
-  var data = new Date();
-  var gg, mm, aaaa;
-  gg = data.getDate() + "/";
-  mm = data.getMonth() + 1 + "/";
-  aaaa = data.getFullYear();
+  // Check if service is active
+  const options = {
+    url: config.get("homel_2") + "/getStatusServer",
+    method: "get"
+  }
+  
+  axios(options)
+  // If service is active
+  .then(response => {
 
-  const strday = req.body.day.split("-")
+    // I create data for amqp producer
+    var data = new Date();
+    var gg, mm, aaaa;
+    gg = data.getDate() + "/";
+    mm = data.getMonth() + 1 + "/";
+    aaaa = data.getFullYear();
+
+    const strday = req.body.day.split("-")
     const day = strday[2]
     const month = strday[1]
     const year = strday[0]
 
-  var objToSend = {
-    name: req.body.name,
-    surname: req.body.surname,
-    email: req.body.email,
-    day: day + "/" + month + "/" + year,
-    street: req.body.street,
-    cash: req.body.cash,
-    service: "2",
-    lang: "ita"
-  };
-
-  if (config.get("security_active") == "yes") {
-    const options = {
-      url: config.get("homel_2") + "/getStatusServer",
-      method: "get",
+    var objToSend = {
+      name: req.body.name,
+      surname: req.body.surname,
+      email: req.body.email,
+      day: day + "/" + month + "/" + year,
+      street: req.body.street,
+      cash: req.body.cash,
+      service: "2",
+      lang: "ita"
     };
 
-    axios(options)
-      .then((response) => {
-        // Getting publicKeys array
-        const { publicKeys } = require("../startup/getPublicKey");
+    // If security is active
+    if (config.get("security_active") == "yes") {
+      
+      // Getting publicKeys array
+      const { publicKeys } = require("../startup/getPublicKey");
 
-        if (response.data.encoding == "symmetric") publicKeys[0] = "symmetric";
+      if (response.data.encoding == "symmetric") publicKeys[0] = "symmetric";
 
-        sendInfoToOneServer(objToSend);
-        res.redirect(config.get("currentNodeUrl") + config.get("port") +'/it/messageSendingSuccess')
-      })
-      .catch((err) => {
-        // Getting publicKeys array
-        const { publicKeys } = require("../startup/getPublicKey");
+      sendInfoToOneServer(objToSend);
+      res.redirect(config.get("currentNodeUrl") + config.get("port") +'/it/messageSendingSuccess')
+    }
 
-        console.log(config.get("homel_2") + " is offline!");
+    // If security is not active
+    else{
+      sendInfoToOneServer(objToSend);
+      res.redirect(config.get("currentNodeUrl") + config.get("port") +'/it/messageSendingSuccess')
+    }   
+  })
+  
+  // If service is not active
+  .catch(err => {
 
-        publicKeys[0] = "NA";
+    // If security is active
+    if(config.get("security_active") == "yes"){
+      
+      // Getting publicKeys array
+      const { publicKeys } = require("../startup/getPublicKey");
 
-        res.redirect(config.get("currentNodeUrl") + config.get("port") +'/it/messageSendingError')
-      });
-  } else {
-    sendInfoToOneServer(objToSend);
-    res.send(objToSend);
-  }
+      console.log(config.get("homel_2") + " is offline!");
+
+      publicKeys[0] = "NA";
+    }
+
+    res.redirect(config.get("currentNodeUrl") + config.get("port") +'/it/messageSendingError')
+  })
 });
 
 module.exports = router;
