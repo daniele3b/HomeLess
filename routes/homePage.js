96f701438f95ed5e2e83e711423bcb0d56ed3cd7
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2020-08-10 16:12:56 +0200

    added arabic frontend for homePage, services choice, service 2 and 3

--- routes/homePage.js
+++ routes/homePage.js
@@ -2,12 +2,10 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
+// English
+
 router.get("/en", async (req, res) => {
   res.sendFile(path.join(__dirname, "../views/en", "home.html"));
-});
-
-router.get("/it", async (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/it", "home.html"));
 });
 
 router.get("/en/service1", async (req, res) => {
@@ -20,6 +18,28 @@ router.get("/en/service2", async (req, res) => {
 
 router.get("/en/service3", async (req, res) => {
   res.sendFile(path.join(__dirname, "../views/en", "home3.html"));
+});
+
+router.get("/en/messageSendingSuccess", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/en", "messageSendingSuccess.html"));
+});
+
+router.get("/en/messageSendingError", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/en", "messageSendingError.html"));
+});
+
+router.get("/en/messageVerifyDocumentError", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/en", "messageVerifyDocumentError.html"));
+});
+
+router.get("/en/messageVerifyDocumentSuccess", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/en", "messageVerifyDocumentSuccess.html"));
+});
+
+// Italian
+
+router.get("/it", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/it", "home.html"));
 });
 
 router.get("/it/service1", async (req, res) => {
@@ -38,32 +58,50 @@ router.get("/it/messageSendingSuccess", async (req, res) => {
   res.sendFile(path.join(__dirname, "../views/it", "messageSendingSuccess.html"));
 });
 
-router.get("/en/messageSendingSuccess", async (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/en", "messageSendingSuccess.html"));
-});
-
 router.get("/it/messageSendingError", async (req, res) => {
   res.sendFile(path.join(__dirname, "../views/it", "messageSendingError.html"));
-});
-
-router.get("/en/messageSendingError", async (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/en", "messageSendingError.html"));
-});
-
-router.get("/en/messageVerifyDocumentError", async (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/en", "messageVerifyDocumentError.html"));
 });
 
 router.get("/it/messageVerifyDocumentError", async (req, res) => {
   res.sendFile(path.join(__dirname, "../views/it", "messageVerifyDocumentError.html"));
 });
 
-router.get("/en/messageVerifyDocumentSuccess", async (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/en", "messageVerifyDocumentSuccess.html"));
-});
-
 router.get("/it/messageVerifyDocumentSuccess", async (req, res) => {
   res.sendFile(path.join(__dirname, "../views/it", "messageVerifyDocumentSuccess.html"));
+});
+
+// Arabic
+
+router.get("/arb", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/arb", "home.html"));
+});
+
+router.get("/arb/service1", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/arb", "home1.html"));
+});
+
+router.get("/arb/service2", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/arb", "home2.html"));
+});
+
+router.get("/arb/service3", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/arb", "home3.html"));
+});
+
+router.get("/arb/messageSendingSuccess", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/arb", "messageSendingSuccess.html"));
+});
+
+router.get("/arb/messageSendingError", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/arb", "messageSendingError.html"));
+});
+
+router.get("/arb/messageVerifyDocumentError", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/arb", "messageVerifyDocumentError.html"));
+});
+
+router.get("/arb/messageVerifyDocumentSuccess", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/arb", "messageVerifyDocumentSuccess.html"));
 });
 
 module.exports = router;
--- routes/reqToHL2.js
+++ routes/reqToHL2.js
@@ -148,4 +148,75 @@ router.post("/it/service2", async (req, res) => {
   })
 });
 
+router.post("/arb/service2", async (req, res) => {
+  // Check if service is active
+  const options = {
+    url: config.get("homel_2") + "/getStatusServer",
+    method: "get"
+  }
+  
+  axios(options)
+  // If service is active
+  .then(response => {
+
+    // I create data for amqp producer
+    var data = new Date();
+    var gg, mm, aaaa;
+    gg = data.getDate() + "/";
+    mm = data.getMonth() + 1 + "/";
+    aaaa = data.getFullYear();
+
+    const strday = req.body.day.split("-")
+    const day = strday[2]
+    const month = strday[1]
+    const year = strday[0]
+
+    var objToSend = {
+      name: req.body.name,
+      surname: req.body.surname,
+      email: req.body.email,
+      day: day + "/" + month + "/" + year,
+      street: req.body.street,
+      cash: req.body.cash,
+      service: "2",
+      lang: "arb"
+    };
+
+    // If security is active
+    if (config.get("security_active") == "yes") {
+      
+      // Getting publicKeys array
+      const { publicKeys } = require("../startup/getPublicKey");
+
+      if (response.data.encoding == "symmetric") publicKeys[0] = "symmetric";
+
+      sendInfoToOneServer(objToSend);
+      res.redirect(config.get("currentNodeUrl") + config.get("port") +'/arb/messageSendingSuccess')
+    }
+
+    // If security is not active
+    else{
+      sendInfoToOneServer(objToSend);
+      res.redirect(config.get("currentNodeUrl") + config.get("port") +'/arb/messageSendingSuccess')
+    }   
+  })
+  
+  // If service is not active
+  .catch(err => {
+
+    // If security is active
+    if(config.get("security_active") == "yes"){
+      
+      // Getting publicKeys array
+      const { publicKeys } = require("../startup/getPublicKey");
+
+      console.log(config.get("homel_2") + " is offline!");
+
+      publicKeys[0] = "NA";
+    }
+
+    res.redirect(config.get("currentNodeUrl") + config.get("port") +'/arb/messageSendingError')
+  })
+});
+
 module.exports = router;
