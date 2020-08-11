9a161e7381527d45ea971585c7d1edc25b62fa84
Mario <cavaiola.1794586@studenti.uniroma1.it>
2020-08-11 20:29:45 +0200

    خدمة واحدة باللغة العربية

--- routes/reqToHL1.js
+++ routes/reqToHL1.js
@@ -95,4 +95,52 @@ router.post("/it/service1", async (req, res) => {
   
 });
 
+router.post("/arb/service1", async (req, res) => {
+
+  const options = {
+      url: config.get("homel_1") + "/getStatusServer",
+      method: "get",
+  };
+  
+  axios(options)
+  .then(response => {
+    var data = new Date();
+    var gg, mm, aaaa;
+    gg = data.getDate() + "/";
+    mm = data.getMonth() + 1 + "/";
+    aaaa = data.getFullYear();
+    
+    
+    const birthday = req.body.birthday.split("-")
+    const birthday_day = birthday[2]
+    const birthday_month = birthday[1]
+    const birthday_year = birthday[0]
+    
+  
+    var objToSend = {
+      name: req.body.name,
+      surname: req.body.surname,
+      email: req.body.email,
+      birthday: birthday_day + "/" + birthday_month + "/" + birthday_year,
+      day: gg + mm + aaaa,
+      street: req.body.street,
+      city: req.body.city,
+      cap: req.body.cap,
+      to: req.body.to,
+      service: "1",
+      lang: "arb"
+    };
+  
+    sendInfoToOneServer(objToSend);
+  
+    res.redirect(config.get("currentNodeUrl") + config.get("port") +'/en/messageSendingSuccess')
+  })
+  .catch(err => {
+    res.redirect(config.get("currentNodeUrl") + config.get("port") +'/en/messageSendingError')
+  })
+
+  
+});
+
+
 module.exports = router;
