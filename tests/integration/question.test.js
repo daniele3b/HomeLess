f70d36567fce50dd00268a83f2ad6d3d9ed21a3b
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2020-09-16 21:42:05 +0200

    finished integration tests for question.js module

--- tests/integration/question.test.js
+++ tests/integration/question.test.js
@@ -25,7 +25,7 @@ describe('Question', () => {
         await mongoose.connection.close()
     })
 
-    describe("/getQuestion/:question_id/:service/:language", () => {
+    describe("GET /getQuestion/:question_id/:service/:language", () => {
 
         let service
         let language
@@ -114,7 +114,7 @@ describe('Question', () => {
         
     })
 
-    describe("/getTreeFrom/:question_id/:service/:language", () => {
+    describe("GET /getTreeFrom/:question_id/:service/:language", () => {
 
         let service
         let language
@@ -270,7 +270,7 @@ describe('Question', () => {
         
     })
 
-    describe("/getPath/from/:questionStartId/to/:questionEndId/:service/:language", () => {
+    describe("GET /getPath/from/:questionStartId/to/:questionEndId/:service/:language", () => {
 
         let service
         let language
@@ -516,7 +516,7 @@ describe('Question', () => {
         
     })
 
-    describe("/getRemovableQuestions/:service/:language", () => {
+    describe("GET /getRemovableQuestions/:service/:language", () => {
 
         let service
         let language
@@ -636,7 +636,7 @@ describe('Question', () => {
         
     })
 
-    describe("/addQuestion/:service/:language", () => {
+    describe("POST /addQuestion/:service/:language", () => {
 
         let service
         let language
@@ -680,7 +680,7 @@ describe('Question', () => {
             expect(res.status).toBe(400)
         })
 
-        /*it("should return 400 if question_id is less than 6 characters", async () => {
+        it("should return 400 if question_id is less than 6 characters", async () => {
             question_id = "Q1_IT"
 
             const res = await exec()
@@ -694,7 +694,7 @@ describe('Question', () => {
             const res = await exec()
     
             expect(res.status).toBe(400)
-        })*/
+        })
 
         it("should return 404 if service is not found", async () => {
             question_id = "Q1_ENG"
@@ -750,11 +750,11 @@ describe('Question', () => {
             expect(questionAdded.text).toBe("First question")
         })
 
-        it("should insert a new root question for service", async () => {
+        it("should insert a new root question for service 2", async () => {
             let q1 = new QuestionService2({
                 question_id: 'Q1_ENG',
                 language: "ENG",
-                text: "First question",
+                text: "Old root question",
                 previousQuestion: null,
                 nextQuestions: [],
                 pathPreviewPdf: "",
@@ -765,7 +765,7 @@ describe('Question', () => {
 
             nextQuestion = q1.question_id
             previousQuestion = "NaN"
-            text = "Second question"
+            text = "New root question"
             question_id = "Q2_ENG"
             language = "ENG"
             service = "2"
@@ -784,11 +784,486 @@ describe('Question', () => {
             expect(root.nextQuestions.length).toBe(1)
             expect(root.nextQuestions[0].nextQuestionId).toBe("Q1_ENG")
             expect(root.previousQuestion).toBe(null)
-            expect(root.text).toBe("Second question")
+            expect(root.text).toBe("New root question")
 
             expect(oldRoot.previousQuestion).toBe("Q2_ENG")
         })
+
+        it("[no template_id] should insert a new leaf question for service 2", async () => {
+            let q1 = new QuestionService2({
+                question_id: 'Q1_ENG',
+                language: "ENG",
+                text: "Root question",
+                previousQuestion: null,
+                nextQuestions: [],
+                pathPreviewPdf: "",
+                template_id: null
+            })
+            
+            await q1.save()
+
+            nextQuestion = "NaN"
+            previousQuestion = q1.question_id
+            text = "Leaf question"
+            question_id = "Q2_ENG"
+            language = "ENG"
+            service = "2"
+            answer = "Go to Q2"
+
+            const res = await exec()
+
+            let root = await QuestionService2.find({question_id: "Q1_ENG", language: language})
+            root = root[0]
+
+            const rootOldNextQuestionsLength = q1.nextQuestions.length
+
+            let leaf = await QuestionService2.find({question_id: "Q2_ENG", language: language})
+            leaf = leaf[0]
+
+            expect(res.status).toBe(200)
+            expect(leaf.question_id).toBe("Q2_ENG")
+            expect(leaf.language).toBe("ENG")
+            expect(leaf.nextQuestions.length).toBe(0)
+            expect(leaf.previousQuestion).toBe("Q1_ENG")
+            expect(leaf.text).toBe("Leaf question")
+
+            const rootNewNextQuestionsLength = rootOldNextQuestionsLength + 1
+            expect(root.nextQuestions.length).toBe(rootNewNextQuestionsLength)
+            expect(root.nextQuestions[rootNewNextQuestionsLength - 1].nextQuestionId).toBe("Q2_ENG")
+            expect(root.nextQuestions[rootNewNextQuestionsLength - 1].answer).toBe("Go to Q2")
+        })
+
+        it("[with template_id] should insert a new leaf question for service 2", async () => {
+            let q1 = new QuestionService2({
+                question_id: 'Q1_ENG',
+                language: "ENG",
+                text: "Root question",
+                previousQuestion: null,
+                nextQuestions: [{nextQuestionId: "Q2_ENG", answer: "Go to Q2"}],
+                pathPreviewPdf: "",
+                template_id: null
+            })
+            
+            await q1.save()
+
+            let q2 = new QuestionService2({
+                question_id: 'Q2_ENG',
+                language: "ENG",
+                text: "Old leaf question",
+                previousQuestion: "Q1_ENG",
+                nextQuestions: [],
+                pathPreviewPdf: "preview_2.jpg",
+                template_id: "T2"
+            })
+            
+            await q2.save()
+
+            nextQuestion = "NaN"
+            previousQuestion = q2.question_id
+            text = "New Leaf question"
+            question_id = "Q3_ENG"
+            language = "ENG"
+            service = "2"
+            template_id = "T3"
+            pathPreviewPdf = "preview_3.jpg"
+            answer = "Go to Q3"
+
+            const res = await exec()
+
+            let oldLeaf = await QuestionService2.find({question_id: "Q2_ENG", language: language})
+            oldLeaf = oldLeaf[0]
+
+            let newLeaf = await QuestionService2.find({question_id: "Q3_ENG", language: language})
+            newLeaf = newLeaf[0]
+
+            expect(res.status).toBe(200)
+
+            expect(newLeaf.question_id).toBe("Q3_ENG")
+            expect(newLeaf.language).toBe("ENG")
+            expect(newLeaf.nextQuestions.length).toBe(0)
+            expect(newLeaf.previousQuestion).toBe("Q2_ENG")
+            expect(newLeaf.text).toBe("New Leaf question")
+            expect(newLeaf.template_id).toBe("T3")
+            expect(newLeaf.pathPreviewPdf).toBe("preview_3.jpg")
+
+            const oldLeafOldNextQuestionLength = q2.nextQuestions.length
+
+            const oldLeafNewNextQuestionLength = oldLeafOldNextQuestionLength + 1
+            expect(oldLeaf.nextQuestions.length).toBe(oldLeafNewNextQuestionLength)
+            expect(oldLeaf.nextQuestions[oldLeafNewNextQuestionLength - 1].nextQuestionId).toBe("Q3_ENG")
+            expect(oldLeaf.nextQuestions[oldLeafNewNextQuestionLength - 1].answer).toBe("Go to Q3")
+            expect(oldLeaf.pathPreviewPdf).toBe("")
+            expect(oldLeaf.template_id).toBe(null)
+        })
+
+        it("should return 400 if question for service 2 with the same template_id is found in DB", async () => {
+            let q1 = new QuestionService2({
+                question_id: 'Q1_ENG',
+                language: "ENG",
+                text: "Root question",
+                previousQuestion: null,
+                nextQuestions: [{nextQuestionId: "Q2_ENG", answer: "Go to Q2"}],
+                pathPreviewPdf: "",
+                template_id: null
+            })
+            
+            await q1.save()
+
+            let q2 = new QuestionService2({
+                question_id: 'Q2_ENG',
+                language: "ENG",
+                text: "Old leaf question",
+                previousQuestion: "Q1_ENG",
+                nextQuestions: [],
+                pathPreviewPdf: "preview_2.jpg",
+                template_id: "T2"
+            })
+            
+            await q2.save()
+
+            nextQuestion = "NaN"
+            previousQuestion = q2.question_id
+            text = "New Leaf question"
+            question_id = "Q3_ENG"
+            language = "ENG"
+            service = "2"
+            template_id = "T2"
+            pathPreviewPdf = "preview_3.jpg"
+
+            const res = await exec()
+
+            expect(res.status).toBe(400)
+        })
+
+        it("should return 400 if it is impossible to create a link between the two questions specified for service 2", async () => {
+            let q1 = new QuestionService2({
+                question_id: 'Q1_ENG',
+                language: "ENG",
+                text: "Root question",
+                previousQuestion: null,
+                nextQuestions: [{nextQuestionId: "Q2_ENG", answer: "Go to Q2"}, 
+                    {nextQuestionId: "Q3_ENG", answer: "Go to Q3"}],
+                pathPreviewPdf: "",
+                template_id: null
+            })
+            
+            await q1.save()
+
+            let q2 = new QuestionService2({
+                question_id: 'Q2_ENG',
+                language: "ENG",
+                text: "Q2 leaf question",
+                previousQuestion: "Q1_ENG",
+                nextQuestions: [],
+                pathPreviewPdf: "preview_2.jpg",
+                template_id: "T2"
+            })
+            
+            await q2.save()
+
+            let q3 = new QuestionService2({
+                question_id: 'Q3_ENG',
+                language: "ENG",
+                text: "Q3 leaf question",
+                previousQuestion: "Q1_ENG",
+                nextQuestions: [],
+                pathPreviewPdf: "preview_3.jpg",
+                template_id: "T3"
+            })
+            
+            await q3.save()
+
+            nextQuestion = q3.question_id
+            previousQuestion = q2.question_id
+            text = "New question"
+            question_id = "Q4_ENG"
+            language = "ENG"
+            service = "2"
+
+            const res = await exec()
+
+            expect(res.status).toBe(400)
+        })
+
+        it("should insert a new question between two questions", async () => {
+            let q1 = new QuestionService2({
+                question_id: 'Q1_ENG',
+                language: "ENG",
+                text: "Root question",
+                previousQuestion: null,
+                nextQuestions: [{nextQuestionId: "Q2_ENG", answer: "Go to Q2"}],
+                pathPreviewPdf: "",
+                template_id: null
+            })
+            
+            await q1.save()
+
+            let q2 = new QuestionService2({
+                question_id: 'Q2_ENG',
+                language: "ENG",
+                text: "Q2 leaf question",
+                previousQuestion: "Q1_ENG",
+                nextQuestions: [],
+                pathPreviewPdf: "preview_2.jpg",
+                template_id: "T2"
+            })
+            
+            await q2.save()
+
+            nextQuestion = q2.question_id
+            previousQuestion = q1.question_id
+            text = "New question"
+            question_id = "Q3_ENG"
+            language = "ENG"
+            service = "2"
+            answer = "Go to Q3"
+            
+            const res = await exec()
+            expect(res.status).toBe(200)
+
+            let prev = await QuestionService2.find({question_id: q1.question_id, language: language})
+            prev = prev[0]
+
+            let added = await QuestionService2.find({question_id: question_id, language: language})
+            added = added[0]
+
+            let next = await QuestionService2.find({question_id: q2.question_id, language: language})
+            next = next[0]
+
+            expect(prev.nextQuestions.some(q => q.nextQuestionId == added.question_id)).toBeTruthy()
+            expect(prev.nextQuestions.some(q => q.answer == "Go to Q3")).toBeTruthy()
+            
+            expect(added.question_id).toBe("Q3_ENG")
+            expect(added.previousQuestion).toBe("Q1_ENG")
+            expect(added.nextQuestions.some(q => q.nextQuestionId == "Q2_ENG")).toBeTruthy()
+            expect(added.text).toBe("New question")
+            expect(added.language).toBe(language)
+
+            expect(next.previousQuestion).toBe("Q3_ENG")
+        })
         
+    })
+
+    describe("DELETE /removeQuestion/:question_id/:service/:language", async () => {
+        let service
+        let language
+        let question_id
+
+        const exec = async () => {
+            return await request(server)
+                .delete("/removeQuestion/"+question_id+"/"+service+"/"+language)
+        }
+
+        it("should return 400 if language is less than 3 characthers", async () => {
+            language = "IT"
+
+            const res = await exec()
+    
+            expect(res.status).toBe(400)
+        })
+
+        it("should return 400 if language is more than 3 characthers", async () => {
+            language = "ENGL"
+
+            const res = await exec()
+    
+            expect(res.status).toBe(400)
+        })
+
+        it("should return 400 if question_id is less than 6 characters", async () => {
+            question_id = "Q1_IT"
+
+            const res = await exec()
+    
+            expect(res.status).toBe(400)
+        })
+
+        it("should return 400 if question_id is more than 7 characters", async () => {
+            question_id = "Q1_ENGL"
+
+            const res = await exec()
+    
+            expect(res.status).toBe(400)
+        })
+
+        it("should return 404 if service is not found", async () => {
+            language = "ENG"
+            service = "not_a_service"
+            question_id = "Q1_ENG"
+
+            const res = await exec()
+    
+            expect(res.status).toBe(404)
+        })
+
+        it("should return 404 if question for service 2 with the given id is not found in DB", async () => {
+            question_id = "Q1_ENG"
+            language = "ENG"
+            service = "2"
+
+            const res = await exec()
+
+            expect(res.status).toBe(404)
+        })
+
+        it("should delete all tree if question_id for service 2 is tree's root", async () => {
+            let q1 = new QuestionService2({
+                question_id: 'Q1_ENG',
+                language: "ENG",
+                text: "Root question",
+                previousQuestion: null,
+                nextQuestions: [{nextQuestionId: "Q2_ENG", answer: "Go to Q2"}, 
+                    {nextQuestionId: "Q3_ENG", answer: "Go to Q3"}],
+                pathPreviewPdf: "",
+                template_id: null
+            })
+            
+            await q1.save()
+
+            let q2 = new QuestionService2({
+                question_id: 'Q2_ENG',
+                language: "ENG",
+                text: "Q2 leaf question",
+                previousQuestion: "Q1_ENG",
+                nextQuestions: [],
+                pathPreviewPdf: "preview_2.jpg",
+                template_id: "T2"
+            })
+            
+            await q2.save()
+
+            let q3 = new QuestionService2({
+                question_id: 'Q3_ENG',
+                language: "ENG",
+                text: "Q3 leaf question",
+                previousQuestion: "Q1_ENG",
+                nextQuestions: [],
+                pathPreviewPdf: "preview_3.jpg",
+                template_id: "T3"
+            })
+            
+            await q3.save()
+
+            question_id = "Q1_ENG"
+            language = "ENG"
+            service = "2"
+
+            const res = await exec()
+
+            expect(res.status).toBe(200)
+            
+            const tree = await QuestionService2.find({language: language})
+
+            expect(tree.length).toBe(0)
+        })
+
+    })
+
+    describe("/modifyQuestion/:question_id/:service/:language", () => {
+        let service
+        let language
+        let question_id
+        let text
+        let nextQuestions
+        let template_id
+
+        const exec = async () => {
+            return await request(server)
+                .put("/modifyQuestion/"+question_id+"/"+service+"/"+language)
+                .send({
+                    text: text,
+                    nextQuestions: nextQuestions,
+                    template_id: template_id
+                })
+        }
+
+        it("should return 400 if language is less than 3 characthers", async () => {
+            language = "IT"
+
+            const res = await exec()
+    
+            expect(res.status).toBe(400)
+        })
+
+        it("should return 400 if language is more than 3 characthers", async () => {
+            language = "ENGL"
+
+            const res = await exec()
+    
+            expect(res.status).toBe(400)
+        })
+
+        it("should return 400 if question_id is less than 6 characters", async () => {
+            question_id = "Q1_IT"
+
+            const res = await exec()
+    
+            expect(res.status).toBe(400)
+        })
+
+        it("should return 400 if question_id is more than 7 characters", async () => {
+            question_id = "Q1_ENGL"
+
+            const res = await exec()
+    
+            expect(res.status).toBe(400)
+        })
+
+        it("should return 404 if service is not found", async () => {
+            language = "ENG"
+            service = "not_a_service"
+            question_id = "Q1_ENG"
+
+            const res = await exec()
+    
+            expect(res.status).toBe(404)
+        })
+
+        it("should return 404 if question for service 2 with the given id is not found in DB", async () => {
+            question_id = "Q1_ENG"
+            language = "ENG"
+            service = "2"
+
+            const res = await exec()
+
+            expect(res.status).toBe(404)
+        })
+
+        it("should update the question with the given id for service 2", async () => {
+            let q1 = new QuestionService2({
+                question_id: 'Q1_ENG',
+                language: "ENG",
+                text: "First question",
+                previousQuestion: null,
+                nextQuestions: [{nextQuestionId: "Q2_ENG", answer: "Go to Q2"}, 
+                    {nextQuestionId: "Q3_ENG", answer: "Go to Q3"}],
+                pathPreviewPdf: "",
+                template_id: null
+            })
+            
+            await q1.save()
+
+            question_id = "Q1_ENG"
+            language = "ENG"
+            service = "2"
+
+            text = "Root question"
+            nextQuestions = [{nextQuestionId: "Q2_ENG", answer: "Go to question 2"}, 
+                {nextQuestionId: "Q3_ENG", answer: "Go to question 3"}]
+            template_id = "T1"
+
+            const res = await exec()
+
+            expect(res.status).toBe(200)
+
+            let updatedQuestion = await QuestionService2.find({question_id: question_id, language: language})
+            updatedQuestion = updatedQuestion[0]
+
+            expect(updatedQuestion.text).toBe("Root question")
+            expect(updatedQuestion.nextQuestions[0].answer).toBe("Go to question 2")
+            expect(updatedQuestion.nextQuestions[1].answer).toBe("Go to question 3")
+            expect(updatedQuestion.template_id).toBe("T1")
+        })
     })
 
 })
