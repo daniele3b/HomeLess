535f8b882d90bba3a0345cbc8e0c26e4851be52e
daniele3b <bufalieri.1794424@studenti.uniroma1.it>
2020-09-17 09:24:57 +0200

    fixed test

--- tests/integration/networkNode.test.js
+++ tests/integration/networkNode.test.js
@@ -1,243 +1,251 @@
-const request = require('supertest')
-const config = require("config")
+const request = require("supertest");
+const config = require("config");
 
-let server
+let server;
 
-
-describe('/networkNode', () => {
-    
+if (config.get("blockChainActive") == "yes") {
+  describe("/networkNode", () => {
     beforeEach(() => {
-        server = require('../../index')
-    })
-        
+      server = require("../../index");
+    });
+
     afterEach(async () => {
-        await server.close()
-    })
+      await server.close();
+    });
 
-    async function getLedger(){
-        const res = await request(server)
-            .get('/blockchain')
+    async function getLedger() {
+      const res = await request(server).get("/blockchain");
 
-        return res.body
+      return res.body;
     }
-    
+
     it("should return the blockchain of the current node", async () => {
-        const res = await request(server)
-                .get('/blockchain')
+      const res = await request(server).get("/blockchain");
 
-        const ledger = res.body
+      const ledger = res.body;
 
-        expect(res.status).toBe(200);
-        expect(ledger.chain.length).toBe(1)
-        expect(ledger.chain[0].index).toBe(1)
-        expect(ledger.chain[0].transactions.length).toBe(0)
-        expect(ledger.chain[0].hash).toBe("0")
-        expect(ledger.chain[0].previousBlockHash).toBe("0")
-        expect(ledger.currentNodeUrl).toBe(config.get("currentNodeUrl") + config.get("port"))
-    })
+      expect(res.status).toBe(200);
+      expect(ledger.chain.length).toBe(1);
+      expect(ledger.chain[0].index).toBe(1);
+      expect(ledger.chain[0].transactions.length).toBe(0);
+      expect(ledger.chain[0].hash).toBe("0");
+      expect(ledger.chain[0].previousBlockHash).toBe("0");
+      expect(ledger.currentNodeUrl).toBe(
+        config.get("currentNodeUrl") + config.get("port")
+      );
+    });
 
     it("should add a new transaction to the pendingTransaction array of the current node", async () => {
-        let ledger = await getLedger()
-        
-        const oldPendingTransactionsLenght = ledger.pendingTransactions.length
-        
-        const res = await request(server)
-            .post('/transaction')
-            .send({newTransaction: "new transaction"})
+      let ledger = await getLedger();
 
-        ledger = await getLedger()
-        
-        const newPendingTransactionsLenght = ledger.pendingTransactions.length
+      const oldPendingTransactionsLenght = ledger.pendingTransactions.length;
 
-        expect(res.status).toBe(200);
-        expect(newPendingTransactionsLenght).toBe(oldPendingTransactionsLenght + 1)
-        expect(ledger.pendingTransactions[0]).toBe("new transaction")
+      const res = await request(server)
+        .post("/transaction")
+        .send({ newTransaction: "new transaction" });
 
-    })
+      ledger = await getLedger();
+
+      const newPendingTransactionsLenght = ledger.pendingTransactions.length;
+
+      expect(res.status).toBe(200);
+      expect(newPendingTransactionsLenght).toBe(
+        oldPendingTransactionsLenght + 1
+      );
+      expect(ledger.pendingTransactions[0]).toBe("new transaction");
+    });
 
     it("should recieve a new block and push it in the chain if block is valid", async () => {
-        let ledger = await getLedger()
+      let ledger = await getLedger();
 
-        const oldChainLenght = ledger.chain.length
+      const oldChainLenght = ledger.chain.length;
 
-        const newBlock = {
-            index: 2,
-            timestamp: Date.now(),
-            transactions: [{
-                userData: {
-                    name: "Ivan",
-                    surname: "Giacomoni",
-                    id: '2'
-                },
-                signature: "signature",
-                publicKey: "publicKey",
-                transactionId: "A7SYFxd67acr6FCVf"
-            }],
-            hash: "wfwfscffscsf", 
-            previousBlockHash: "0",
-        };
-
-        const res = await request(server)
-                .post('/receive-new-block')
-                .send({newBlock: newBlock})
-
-        ledger = await getLedger()
-        
-        const newChainLenght = ledger.chain.length
-    
-        expect(res.status).toBe(200)
-        expect(newChainLenght).toBe(oldChainLenght + 1)
-    })
-
-    it("should recieve a new block and reject it if block's index is not valid", async () => {
-
-        const newBlock = {
-            index: 10,
-            timestamp: Date.now(),
-            transactions: [{
-                userData: {
-                    name: "Ivan",
-                    surname: "Giacomoni",
-                    id: '2'
-                },
-                signature: "signature",
-                publicKey: "publicKey",
-                transactionId: "A7SYFxd67acr6FCVf"
-            }],
-            hash: "hrterwy5", 
-            previousBlockHash: "0",
-        };
-
-        const res = await request(server)
-                .post('/receive-new-block')
-                .send({newBlock: newBlock})
-    
-        expect(res.status).toBe(400)
-    })
-
-    it("should recieve a new block and reject it if block's previous block hash is not valid", async () => {
-
-        const newBlock = {
-            index: 3,
-            timestamp: Date.now(),
-            transactions: [{
-                userData: {
-                    name: "Ivan",
-                    surname: "Giacomoni",
-                    id: '2'
-                },
-                signature: "signature",
-                publicKey: "publicKey",
-                transactionId: "A7SYFxd67acr6FCVf"
-            }],
-            hash: "hrterwy5", 
-            previousBlockHash: "invalid previous block hash",
-        };
-
-        const res = await request(server)
-                .post('/receive-new-block')
-                .send({newBlock: newBlock})
-    
-        expect(res.status).toBe(400)
-    })
-
-    it("should register a new node with the network", async () => {
-
-        let ledger = await getLedger()
-
-        const oldNetworkNodesLength = ledger.networkNodes.length
-
-        res = await request(server)
-                .post('/register-node')
-                .send({newNodeUrl: "http://localhost:8081"})
-
-        ledger = await getLedger()
-
-        const newNetworkNodesLength = ledger.networkNodes.length
-        
-        expect(res.status).toBe(200)
-        expect(newNetworkNodesLength).toBe(oldNetworkNodesLength + 1)
-        
-    })
-
-    it("should register the current node with multiple new nodes of the network", async () => {
-
-        let ledger = await getLedger()
-
-        const oldNetworkNodesLength = ledger.networkNodes.length
-
-        const allNetworkNodes = ["http://localhost:8083", "http://localhost:8084", "http://localhost:8085"]
-        const allNetworkNodesLength = allNetworkNodes.length
-
-        res = await request(server)
-                .post('/register-nodes-bulk')
-                .send({allNetworkNodes: allNetworkNodes})
-
-        ledger = await getLedger()
-
-        const newNetworkNodesLength = ledger.networkNodes.length
-
-        expect(res.status).toBe(200)
-        expect(newNetworkNodesLength).toBe(oldNetworkNodesLength + allNetworkNodesLength)
-        expect(ledger.networkNodes[2]).toBe("http://localhost:8083")
-        expect(ledger.networkNodes[3]).toBe("http://localhost:8084")
-        expect(ledger.networkNodes[4]).toBe("http://localhost:8085")
-    })
-    
-    it("should return the transaction which contains the given pdfId if it exists", async () => {
-        
-        const pdfId = '1'
-
-        const newTransaction = {
+      const newBlock = {
+        index: 2,
+        timestamp: Date.now(),
+        transactions: [
+          {
             userData: {
-                name: "Ivan",
-                surname: "Giacomoni",
-                id: pdfId
+              name: "Ivan",
+              surname: "Giacomoni",
+              id: "2",
             },
             signature: "signature",
             publicKey: "publicKey",
-            transactionId: "A7SYFxd67acr6FCVf"
-        }
+            transactionId: "A7SYFxd67acr6FCVf",
+          },
+        ],
+        hash: "wfwfscffscsf",
+        previousBlockHash: "0",
+      };
 
-        let res = await request(server)
-            .post('/transaction')
-            .send({newTransaction: newTransaction})
+      const res = await request(server)
+        .post("/receive-new-block")
+        .send({ newBlock: newBlock });
 
-        const newBlock = {
-            index: 3,
-            timestamp: Date.now(),
-            transactions: [newTransaction],
-            hash: "afwgetwgwegegg", 
-            previousBlockHash: "wfwfscffscsf"
-        };
+      ledger = await getLedger();
 
-        res = await request(server)
-            .post('/receive-new-block')
-            .send({newBlock: newBlock})
-        
-        res = await request(server)
-            .get('/getTransaction/'+pdfId)
-        
-        const transaction = res.body
+      const newChainLenght = ledger.chain.length;
 
-        expect(res.status).toBe(200)
-        expect(transaction.userData.name).toBe("Ivan")
-        expect(transaction.userData.surname).toBe("Giacomoni")
-        expect(transaction.userData.id).toBe("1")
-        expect(transaction.signature).toBe("signature")
-        expect(transaction.publicKey).toBe("publicKey")
-        expect(transaction.transactionId).toBe("A7SYFxd67acr6FCVf")
-    })
+      expect(res.status).toBe(200);
+      expect(newChainLenght).toBe(oldChainLenght + 1);
+    });
+
+    it("should recieve a new block and reject it if block's index is not valid", async () => {
+      const newBlock = {
+        index: 10,
+        timestamp: Date.now(),
+        transactions: [
+          {
+            userData: {
+              name: "Ivan",
+              surname: "Giacomoni",
+              id: "2",
+            },
+            signature: "signature",
+            publicKey: "publicKey",
+            transactionId: "A7SYFxd67acr6FCVf",
+          },
+        ],
+        hash: "hrterwy5",
+        previousBlockHash: "0",
+      };
+
+      const res = await request(server)
+        .post("/receive-new-block")
+        .send({ newBlock: newBlock });
+
+      expect(res.status).toBe(400);
+    });
+
+    it("should recieve a new block and reject it if block's previous block hash is not valid", async () => {
+      const newBlock = {
+        index: 3,
+        timestamp: Date.now(),
+        transactions: [
+          {
+            userData: {
+              name: "Ivan",
+              surname: "Giacomoni",
+              id: "2",
+            },
+            signature: "signature",
+            publicKey: "publicKey",
+            transactionId: "A7SYFxd67acr6FCVf",
+          },
+        ],
+        hash: "hrterwy5",
+        previousBlockHash: "invalid previous block hash",
+      };
+
+      const res = await request(server)
+        .post("/receive-new-block")
+        .send({ newBlock: newBlock });
+
+      expect(res.status).toBe(400);
+    });
+
+    it("should register a new node with the network", async () => {
+      let ledger = await getLedger();
+
+      const oldNetworkNodesLength = ledger.networkNodes.length;
+
+      res = await request(server)
+        .post("/register-node")
+        .send({ newNodeUrl: "http://localhost:8081" });
+
+      ledger = await getLedger();
+
+      const newNetworkNodesLength = ledger.networkNodes.length;
+
+      expect(res.status).toBe(200);
+      expect(newNetworkNodesLength).toBe(oldNetworkNodesLength + 1);
+    });
+
+    it("should register the current node with multiple new nodes of the network", async () => {
+      let ledger = await getLedger();
+
+      const oldNetworkNodesLength = ledger.networkNodes.length;
+
+      const allNetworkNodes = [
+        "http://localhost:8083",
+        "http://localhost:8084",
+        "http://localhost:8085",
+      ];
+      const allNetworkNodesLength = allNetworkNodes.length;
+
+      res = await request(server)
+        .post("/register-nodes-bulk")
+        .send({ allNetworkNodes: allNetworkNodes });
+
+      ledger = await getLedger();
+
+      const newNetworkNodesLength = ledger.networkNodes.length;
+
+      expect(res.status).toBe(200);
+      expect(newNetworkNodesLength).toBe(
+        oldNetworkNodesLength + allNetworkNodesLength
+      );
+      expect(ledger.networkNodes[2]).toBe("http://localhost:8083");
+      expect(ledger.networkNodes[3]).toBe("http://localhost:8084");
+      expect(ledger.networkNodes[4]).toBe("http://localhost:8085");
+    });
+
+    it("should return the transaction which contains the given pdfId if it exists", async () => {
+      const pdfId = "1";
+
+      const newTransaction = {
+        userData: {
+          name: "Ivan",
+          surname: "Giacomoni",
+          id: pdfId,
+        },
+        signature: "signature",
+        publicKey: "publicKey",
+        transactionId: "A7SYFxd67acr6FCVf",
+      };
+
+      let res = await request(server)
+        .post("/transaction")
+        .send({ newTransaction: newTransaction });
+
+      const newBlock = {
+        index: 3,
+        timestamp: Date.now(),
+        transactions: [newTransaction],
+        hash: "afwgetwgwegegg",
+        previousBlockHash: "wfwfscffscsf",
+      };
+
+      res = await request(server)
+        .post("/receive-new-block")
+        .send({ newBlock: newBlock });
+
+      res = await request(server).get("/getTransaction/" + pdfId);
+
+      const transaction = res.body;
+
+      expect(res.status).toBe(200);
+      expect(transaction.userData.name).toBe("Ivan");
+      expect(transaction.userData.surname).toBe("Giacomoni");
+      expect(transaction.userData.id).toBe("1");
+      expect(transaction.signature).toBe("signature");
+      expect(transaction.publicKey).toBe("publicKey");
+      expect(transaction.transactionId).toBe("A7SYFxd67acr6FCVf");
+    });
 
     it("should return 404 if no transaction with the given pdfId is found in the chain", async () => {
-        
-        const pdfId = '10'
+      const pdfId = "10";
 
-        res = await request(server)
-                .get('/getTransaction/'+pdfId)
-        
-    
-        expect(res.status).toBe(404)
-    })
-    
-})
+      res = await request(server).get("/getTransaction/" + pdfId);
+
+      expect(res.status).toBe(404);
+    });
+  });
+} else {
+  describe("IF BLOCKCHAIN IS NOT ACTIVE", () => {
+    it("IT SHOULD PASS BECAUSE NO TEST", () => {
+      expect(true).toBe(true);
+    });
+  });
+}
--- tests/unit/blockchain.test.js
+++ tests/unit/blockchain.test.js
@@ -1,173 +1,177 @@
-const Blockchain = require("../../blockchain/blockchain")
+const Blockchain = require("../../blockchain/blockchain");
+const config = require("config");
 
-describe("createNewBlock", () => {
-    
+if (config.get("blockChainActive") == "yes") {
+  describe("createNewBlock", () => {
     let ledger = new Blockchain();
 
-    beforeEach( () => {
-        ledger.pendingTransactions.push("First transaction")
-        ledger.pendingTransactions.push("Second transaction")
-    })
+    beforeEach(() => {
+      ledger.pendingTransactions.push("First transaction");
+      ledger.pendingTransactions.push("Second transaction");
+    });
 
     it("should create a new block", () => {
-        
-        const oldLength = ledger.chain.length
-        
-        ledger.createNewBlock("prevHash", "hash")
+      const oldLength = ledger.chain.length;
 
-        
-        const newLength = ledger.chain.length
-        const newBlockHash = ledger.chain[1].hash
-        const prevHash = ledger.chain[1].previousBlockHash
-        const newBlockIndex = ledger.chain[1].index
-        const newBlockTransactions = ledger.chain[1].transactions
+      ledger.createNewBlock("prevHash", "hash");
 
-        expect(oldLength).toBe(newLength - 1)
-        expect(newBlockHash).toBe("hash")
-        expect(prevHash).toBe("prevHash")
-        expect(newBlockIndex).toBe(newLength)
-        expect(newBlockTransactions[0]).toBe("First transaction")
-        expect(newBlockTransactions[1]).toBe("Second transaction")
-        expect(newBlockTransactions.length).toBe(2)
-        expect(ledger.pendingTransactions.length).toBe(0)
-    }) 
-})
+      const newLength = ledger.chain.length;
+      const newBlockHash = ledger.chain[1].hash;
+      const prevHash = ledger.chain[1].previousBlockHash;
+      const newBlockIndex = ledger.chain[1].index;
+      const newBlockTransactions = ledger.chain[1].transactions;
 
-describe("getLastBlock", () => {
+      expect(oldLength).toBe(newLength - 1);
+      expect(newBlockHash).toBe("hash");
+      expect(prevHash).toBe("prevHash");
+      expect(newBlockIndex).toBe(newLength);
+      expect(newBlockTransactions[0]).toBe("First transaction");
+      expect(newBlockTransactions[1]).toBe("Second transaction");
+      expect(newBlockTransactions.length).toBe(2);
+      expect(ledger.pendingTransactions.length).toBe(0);
+    });
+  });
 
+  describe("getLastBlock", () => {
     let ledger = new Blockchain();
 
-    beforeEach( () => {
-        ledger.chain.push("First block")
-        ledger.chain.push("Second block")
-        ledger.chain.push("Third block")
-        ledger.chain.push("Last block")
-    })
+    beforeEach(() => {
+      ledger.chain.push("First block");
+      ledger.chain.push("Second block");
+      ledger.chain.push("Third block");
+      ledger.chain.push("Last block");
+    });
 
     it("should return the last block of the chain", () => {
-        const lastBlock = ledger.getLastBlock()
-        
-        expect(lastBlock).toBe("Last block")
-    })
-})
+      const lastBlock = ledger.getLastBlock();
 
-describe("createNewTransaction", () => {
+      expect(lastBlock).toBe("Last block");
+    });
+  });
 
+  describe("createNewTransaction", () => {
     let ledger = new Blockchain();
 
     it("should create a new transaction and return it", () => {
-        const newTransaction = ledger.createNewTransaction("userData", "signature", "publicKey")
+      const newTransaction = ledger.createNewTransaction(
+        "userData",
+        "signature",
+        "publicKey"
+      );
 
-        expect(newTransaction.userData).toBe("userData")
-        expect(newTransaction.signature).toBe("signature")
-        expect(newTransaction.publicKey).toBe("publicKey")
-    })
-})
+      expect(newTransaction.userData).toBe("userData");
+      expect(newTransaction.signature).toBe("signature");
+      expect(newTransaction.publicKey).toBe("publicKey");
+    });
+  });
 
-describe("addTransactionToPendingTransactions", () => {
-
+  describe("addTransactionToPendingTransactions", () => {
     let ledger = new Blockchain();
 
-    beforeEach( () => {
-        ledger.pendingTransactions.push("First transaction")
-        ledger.pendingTransactions.push("Second transaction")
-    })
+    beforeEach(() => {
+      ledger.pendingTransactions.push("First transaction");
+      ledger.pendingTransactions.push("Second transaction");
+    });
 
     it("should add a transaction to the pendingTransaction array and return the index of the block which will take it", () => {
+      const oldPendingTransactionsLength = ledger.pendingTransactions.length;
 
-        const oldPendingTransactionsLength = ledger.pendingTransactions.length
-        
-        const index = ledger.addTransactionToPendingTransactions("Third transaction")
+      const index = ledger.addTransactionToPendingTransactions(
+        "Third transaction"
+      );
 
-        const newPendingTransactionsLength = ledger.pendingTransactions.length
+      const newPendingTransactionsLength = ledger.pendingTransactions.length;
 
-        expect(newPendingTransactionsLength).toBe(oldPendingTransactionsLength + 1)
-        expect(index).toBe(ledger.chain.length + 1)        
-    })
-})
+      expect(newPendingTransactionsLength).toBe(
+        oldPendingTransactionsLength + 1
+      );
+      expect(index).toBe(ledger.chain.length + 1);
+    });
+  });
 
-describe("hashBlock", () => {
-
+  describe("hashBlock", () => {
     let ledger = new Blockchain();
 
     it("should create a valid hash and return it", () => {
-        const hash = ledger.hashBlock("previousBlockHash", "currentBlockData")
-        expect(hash).toBeTruthy()
-    })
-})
+      const hash = ledger.hashBlock("previousBlockHash", "currentBlockData");
+      expect(hash).toBeTruthy();
+    });
+  });
 
-describe("chainIsValid", () => {
-
+  describe("chainIsValid", () => {
     let ledger = new Blockchain();
 
     afterEach(() => {
-        ledger = new Blockchain()
-    })
+      ledger = new Blockchain();
+    });
 
     it("should return true if the chain is valid", () => {
-        const block1 = {
-            index: 2,
-            timestamp: Date.now(),
-            transactions: ledger.pendingTransactions,
-            hash: "hash1",
-            previousBlockHash: "0"
-        }
+      const block1 = {
+        index: 2,
+        timestamp: Date.now(),
+        transactions: ledger.pendingTransactions,
+        hash: "hash1",
+        previousBlockHash: "0",
+      };
 
-        const block2 = {
-            index: 3,
-            timestamp: Date.now(),
-            transactions: ledger.pendingTransactions,
-            hash: "hash2",
-            previousBlockHash: "hash1"
-        }
+      const block2 = {
+        index: 3,
+        timestamp: Date.now(),
+        transactions: ledger.pendingTransactions,
+        hash: "hash2",
+        previousBlockHash: "hash1",
+      };
 
-        ledger.chain.push(block1)
-        ledger.chain.push(block2) 
+      ledger.chain.push(block1);
+      ledger.chain.push(block2);
 
-        const res = ledger.chainIsValid(ledger.chain)
-        
-        expect(res).toBe(true)
-    })
+      const res = ledger.chainIsValid(ledger.chain);
+
+      expect(res).toBe(true);
+    });
 
     it("should return false if chain's structure is not valid", () => {
-        const block1 = {
-            index: 2,
-            timestamp: Date.now(),
-            transactions: ledger.pendingTransactions,
-            hash: "hash1",
-            previousBlockHash: "0"
-        }
+      const block1 = {
+        index: 2,
+        timestamp: Date.now(),
+        transactions: ledger.pendingTransactions,
+        hash: "hash1",
+        previousBlockHash: "0",
+      };
 
-        const block2 = {
-            index: 3,
-            timestamp: Date.now(),
-            transactions: ledger.pendingTransactions,
-            hash: "hash2",
-            previousBlockHash: "abc"
-        }
+      const block2 = {
+        index: 3,
+        timestamp: Date.now(),
+        transactions: ledger.pendingTransactions,
+        hash: "hash2",
+        previousBlockHash: "abc",
+      };
 
-        ledger.chain.push(block1)
-        ledger.chain.push(block2)
+      ledger.chain.push(block1);
+      ledger.chain.push(block2);
 
-        const res = ledger.chainIsValid(ledger.chain);
-        expect(res).toBe(false)
-        
-    })
+      const res = ledger.chainIsValid(ledger.chain);
+      expect(res).toBe(false);
+    });
 
     it("should return false if genesys block is not valid", () => {
-        
-        ledger.chain[0].previousBlockHash = "1"
-        let res = ledger.chainIsValid(ledger.chain);
-        expect(res).toBe(false)
+      ledger.chain[0].previousBlockHash = "1";
+      let res = ledger.chainIsValid(ledger.chain);
+      expect(res).toBe(false);
 
-        ledger.chain[0].hash = "1"
-        res = ledger.chainIsValid(ledger.chain);
-        expect(res).toBe(false)
+      ledger.chain[0].hash = "1";
+      res = ledger.chainIsValid(ledger.chain);
+      expect(res).toBe(false);
 
-        ledger.chain[0].transactions.push("New transaction")
-        res = ledger.chainIsValid(ledger.chain);
-        expect(res).toBe(false)
-        
-    })
-
-})
+      ledger.chain[0].transactions.push("New transaction");
+      res = ledger.chainIsValid(ledger.chain);
+      expect(res).toBe(false);
+    });
+  });
+} else {
+  describe("IF BLOCKCHAIN IS NOT ACTIVE", () => {
+    it("IT SHOULD PASS BECAUSE NO TEST", () => {
+      expect(true).toBe(true);
+    });
+  });
+}
